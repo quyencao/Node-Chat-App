2357a765be04830de00060e1675558d3c9b4e7bc
quyencao <boy20195@gmail.com>
2017-03-28 11:43:52 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,15 +28,15 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
     
     socket.emit('createMessage', {
        from: 'User',
-       text: jQuery('[name=message]').val() 
+       text: messageTextbox.val()
     }, function (data) {
-
+        messageTextbox.val('');
     });
-
-    jQuery('[name=message]').val('');
 });
 
 var locationButton = jQuery('#send-location');
@@ -46,12 +46,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
